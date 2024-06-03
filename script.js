b24c420d49aba8e799efac4708919bb6fe99e791
mmaniezo <rafael.monaco.maniezo@gmail.com>
2024-06-03 19:17:46 -0300

    fimsec5

--- script.js
+++ script.js
@@ -66,18 +66,18 @@
 function aparecerTexto() {
     document.getElementById("divum").innerHTML = "Socializar com pesoas que estão interessadas no assunto, ou seja, buscando melhorar o mundo e o mar!";
     document.getElementById("divdois").innerHTML = "Ver quantas pessoas estão ajudando o mar, sentindo assim uma gratificação por ver que ainda há esperança.";
-    document.getElementById("divtres").innerHTML = "Informações sobre:";
-    document.getElementById("divquatro").innerHTML = "Informações sobre:";
-    document.getElementById("divcinco").innerHTML = "Informações sobre:";
-    document.getElementById("divseis").innerHTML = "Informações sobre:";
+    document.getElementById("divtres").innerHTML = "Ter a informação de quem são as empresas que você pode contar e chamar para ajudar!";
+    document.getElementById("divquatro").innerHTML = "Uma ampla gama de acesso a dados referentes a poluição marinha vinda de nossa Boia. ";
+    document.getElementById("divcinco").innerHTML = "A união faz a força, se junte a uma de nossas comunidades e faça parte da solução você também!";
+    document.getElementById("divseis").innerHTML = "Oque a as pessoas que usam redes sociais e empresas querem? É visibilidade, e caso ajude o oceano é isso que terá.";
   }
   function reset() {
     document.getElementById("divum").innerHTML = '<img src="images/socializar.png" alt="Socializando com pessoas sobre o mesmo assunto">';
     document.getElementById("divdois").innerHTML = '<img src="images/limpando_praia.png" alt="Criança ajudando na limpeza da praia">';
-    document.getElementById("divtres").innerHTML = "";
-    document.getElementById("divquatro").innerHTML = "";
-    document.getElementById("divcinco").innerHTML = "";
-    document.getElementById("divseis").innerHTML = "";
+    document.getElementById("divtres").innerHTML = '<img src="images/cleanup.png" alt="Empresa que trabalha em ajudar o mar">';
+    document.getElementById("divquatro").innerHTML = '<img src="images/dados.png" alt="Dados vindos da Boia SeaConnect">';
+    document.getElementById("divcinco").innerHTML = '<img src="images/comunidade.png" alt="Se unir a outras pessoas da plataforma">';
+    document.getElementById("divseis").innerHTML = '<img src="images/visibilidade.png" alt="Ganhe visibilidade através do sistema de ranking">';
   }
 
   
