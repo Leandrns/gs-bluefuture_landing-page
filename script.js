e16cba7d4f10c50d28c2d56e438ab1257857691b
mmaniezo <rafael.monaco.maniezo@gmail.com>
2024-06-03 20:50:58 -0300

    alteracao

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
+//Seção 5
 
+function aparecerTexto1() {
+    document.getElementById("divum").innerHTML = "Socializar com pesoas que estão interessadas no assunto, ou seja, buscando melhorar o mundo e o mar!";
+}
+function aparecerTexto2() {
+    document.getElementById("divdois").innerHTML = "Ver quantas pessoas estão ajudando o mar, sentindo assim uma gratificação por ver que ainda há esperança.";
+}
+function aparecerTexto3() {
+    document.getElementById("divtres").innerHTML = "Ter a informação de quem são as empresas que você pode contar e chamar para ajudar!";
+}    
+function aparecerTexto4() {
+    document.getElementById("divquatro").innerHTML = "Uma ampla gama de acesso a dados referentes a poluição marinha vinda de nossa Boia. ";
+}    
+function aparecerTexto5() {
+    document.getElementById("divcinco").innerHTML = "A união faz a força, se junte a uma de nossas comunidades e faça parte da solução você também!";
+}    
+function aparecerTexto6() {
+    document.getElementById("divseis").innerHTML = "Oque a as pessoas que usam redes sociais e empresas querem? É visibilidade, e caso ajude o oceano é isso que terá.";
+}
 
-
-
-
-
-
-
-
+function reset1() {
+    document.getElementById("divum").innerHTML = '<img src="images/socializar.png" alt="Socializando com pessoas sobre o mesmo assunto">';
+}
+function reset2() {
+    document.getElementById("divdois").innerHTML = '<img src="images/limpando_praia.png" alt="Criança ajudando na limpeza da praia">';
+}
+function reset3() {
+    document.getElementById("divtres").innerHTML = '<img src="images/cleanup.png" alt="Empresa que trabalha em ajudar o mar">';
+}
+function reset4() {
+    document.getElementById("divquatro").innerHTML = '<img src="images/dados.png" alt="Dados vindos da Boia SeaConnect">';
+}
+function reset5() {
+    document.getElementById("divcinco").innerHTML = '<img src="images/comunidade.png" alt="Se unir a outras pessoas da plataforma">';
+}
+function reset6() {
+    document.getElementById("divseis").innerHTML = '<img src="images/visibilidade.png" alt="Ganhe visibilidade através do sistema de ranking">';
+}
 
 
 
