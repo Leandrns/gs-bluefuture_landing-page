2d0d595d18f0832c5fab19a2a743ed1f823c3d40
Leandro Souza <leandro2005souza@gmail.com>
2024-06-03 17:16:54 -0300

    Organizando arquivos

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+// MENU DROPDOWN
+const toggleBtn = document.querySelector('.toggle_btn')
+const toggleBtnIcon = document.querySelector('.toggle_btn i')
+const menuDropDown = document.querySelector('.menu_dropdown')
 
+toggleBtn.onclick = function () {
+    menuDropDown.classList.toggle('open')
+
+    const isOpen = menuDropDown.classList.contains('open')
+    toggleBtnIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars'
+}
+
+// CARROSSEL DE IMAGENS
+let currentIndex = 0;
+const items = document.querySelectorAll('.carrosel-item');
+const totalItems = items.length;
+
+function showSlide(index) {
+    items.forEach((item, i) => {
+        item.style.opacity = i === index ? '1' : '0';
+    });
+}
+
+document.querySelector('.prev').addEventListener('click', () => {
+    currentIndex = (currentIndex > 0) ? currentIndex - 1 : totalItems - 1;
+    showSlide(currentIndex);
+});
+
+document.querySelector('.next').addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % totalItems;
+    showSlide(currentIndex);
+});
+
+showSlide(currentIndex);
