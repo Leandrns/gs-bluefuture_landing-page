effa9c43905b34146538fd54ddafddd2e83ba5b1
mmaniezo <rafael.monaco.maniezo@gmail.com>
2024-06-03 18:42:10 -0300

    salvando sec5

--- script.js
+++ script.js
@@ -1,1 +1,83 @@
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Seção 5
+
+function aparecerTexto() {
+    document.getElementById("divum").innerHTML = "Socializar com pesoas que estão interessadas no assunto, ou seja, buscando melhorar o mundo e o mar!";
+    document.getElementById("divdois").innerHTML = "Ver quantas pessoas estão ajudando o mar, sentindo assim uma gratificação por ver que ainda há esperança.";
+    document.getElementById("divtres").innerHTML = "Informações sobre:";
+    document.getElementById("divquatro").innerHTML = "Informações sobre:";
+    document.getElementById("divcinco").innerHTML = "Informações sobre:";
+    document.getElementById("divseis").innerHTML = "Informações sobre:";
+  }
+  function reset() {
+    document.getElementById("divum").innerHTML = '<img src="images/socializar.png" alt="Socializando com pessoas sobre o mesmo assunto">';
+    document.getElementById("divdois").innerHTML = '<img src="images/limpando_praia.png" alt="Criança ajudando na limpeza da praia">';
+    document.getElementById("divtres").innerHTML = "";
+    document.getElementById("divquatro").innerHTML = "";
+    document.getElementById("divcinco").innerHTML = "";
+    document.getElementById("divseis").innerHTML = "";
+  }
+
+  
